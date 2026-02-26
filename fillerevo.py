88730a6010e2c9903922c3a87a751fdf2d0ee4f6
arch_rocko <andres@fontalvo.xyz>
2026-02-26 16:26:22 -0500

    fix: Include some linter suggestions

--- fillerevo.py
+++ fillerevo.py
@@ -1,39 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import json, re
+import json
+import re
 
-animes = json.loads(open('animefillerlist.json','r').read())
+animes = json.loads(open('animefillerlist.json', 'r').read())
 
-## Get a list of years and filler percentage
+# Get a list of years and filler percentage
 percents = np.array([])
 years = np.array([])
 
 for ani in animes:
   if not animes[ani]["Release"]:
     continue
-  year = int(re.findall(r'^\d{4}',animes[ani]["Release"])[0])
+  year = int(re.findall(r'^\d{4}', animes[ani]["Release"])[0])
   years = np.append(years, year)
   total_eps = 0
   filler_eps = 0
   for key in animes[ani]:
     if key == 'Release': continue
     total_eps += len(animes[ani][key])
-    if (re.search('[Cc]anon',key) is not None) and (re.search('[Ff]iller',key) is not None):
+    if (re.search('[Cc]anon', key) is not None) and (re.search('[Ff]iller', key) is not None):
       filler_eps += 0.5*len(animes[ani][key])
-    elif re.search('[Cc]anon',key) is None:
+    elif re.search('[Cc]anon', key) is None:
       filler_eps = len(animes[ani][key])
   if filler_eps/total_eps == 1:
     print(ani)
-  percents = np.append(percents,100*filler_eps/total_eps)
-  
-
+  percents = np.append(percents, 100*filler_eps/total_eps)
 
 
 plt.scatter(years, percents)
 plt.xlabel('Years')
 plt.ylabel('Filler percentage')
 
-## Linear regression to estimate filler evolution
+# Linear regression to estimate filler evolution
 
 a, b = np.polyfit(years, percents, 1)
 
@@ -41,9 +40,6 @@ a, b = np.polyfit(years, percents, 1)
 plt.plot(years, a*years+b, color='red')
 
 
-
 plt.savefig('plot_evolution.png')
 
 plt.show()
-      
-  
